589cb286396c406979d617203ac2907597f890f4
VetN <122635397+VetN@users.noreply.github.com>
2023-05-06 13:21:33 +0300

    made json

---
+++ 5.2.2.1.js
@@ -0,0 +1,39 @@
+// Преобразование JSON в JS-объект
+// Перебором for
+const dataList= `{
+    "list": [
+     {
+      "name": "Petr",
+      "age": "20",
+      "prof": "mechanic"
+     },
+     {
+      "name": "Vova",
+      "age": "60",
+      "prof": "pilot"
+     }
+    ]
+   }`;
+
+
+// Получение данных
+const person = JSON.parse(dataList);
+//console.log(person);
+const res = person.list;
+//console.log(res);
+
+//Запись результатов
+const resAll =[];
+for (let i = 0; i <= res.length-1; i++) {
+    resAll.push ({
+        name: res[i].name,
+        age: Number(res[i].age),
+        prof: res[i].prof,
+    });
+}
+  
+
+    
+//console.log(resAll);
+console.log(`list: [ { name: ${resAll[0].name}, age: ${resAll[0].age}, prof:${resAll[0].prof}},`)
+console.log(`{name:${resAll[1].name}, age: ${resAll[1].age}, prof:${resAll[1].prof}},] `)
---
+++ 5.2.2.js
@@ -0,0 +1,32 @@
+// Преобразование JSON в JS-объект
+const dataList= `{
+    "list": [
+     {
+      "name": "Petr",
+      "age": "20",
+      "prof": "mechanic"
+     },
+     {
+      "name": "Vova",
+      "age": "60",
+      "prof": "pilot"
+     }
+    ]
+   }`;
+
+const resultAll = [];
+// Получение данных
+const person = JSON.parse(dataList);
+//console.log(person)
+const listPersons = person.list;
+listPersons.forEach(function (item) {
+    resultAll.push({
+        name: item.name,
+        age: Number(item.age),
+        prof: item.prof,
+    });
+});
+    
+//console.log(resultAll);
+console.log(`list: [ { name:${resultAll[0].name}, age:${resultAll[0].age}, prof:${resultAll[0].prof}},`);
+console.log(`{ name:${resultAll[1].name}, age:${resultAll[1].age}, prof:${resultAll[1].prof}},]`);
